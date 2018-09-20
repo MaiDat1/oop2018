ade3aeeb25a9df5309ccda2ad76820aebc2110d2
MaiDat1 <43159622+MaiDat1@users.noreply.github.com>
2018-09-20 21:55:37 +0700

    updata

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,38 +1,65 @@
 package week1;
-
 public class Student {
 
-    // TODO: khai báo các thuộc tính cho Student
+    private String name,id,group,email;
 
-    // TODO: khai báo các phương thức getter, setter cho Student
+    public void setName(String name )
+    {
+        this.name = name;
+    }
 
-    /**
-     * Constructor 1
-     */
+    public String getName()
+    {
+        return name;
+    }
+    public void setId (String id )
+    {
+        this.id = id ;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+    public void setGroup( String group)
+    {
+        this.group = group;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public void setEmail( String Email) {
+        this.email = email;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+
     Student() {
-        // TODO:
+
+        name="Student";
+        id = "000 ";
+        group="INT22041";
+        email=" uet@vnu.edu.vn";
     }
 
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
     Student(String n, String sid, String em) {
-        // TODO:
+        name = n;
+        id = sid;
+        group = " INT22041";
+        email = em;
     }
 
-    /**
-     * Constructor 3
-     * @param s
-     */
     Student(Student s) {
-        // TODO:
+        name = s.name;
+        id = s.id;
+        group= s.group;
+        email= s.email;
     }
 
-    String getInfo() {
-        // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+    public void getInfo() {
+
+        System.out.println( "thong tin:" + name +" , "+ id + " ," + group + ", " + email);
     }
 }
