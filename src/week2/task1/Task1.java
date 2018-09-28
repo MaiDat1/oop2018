570155e9d440b08b105a30ba5eb4aa58623abf7e
MaiDat1 <43159622+MaiDat1@users.noreply.github.com>
2018-09-28 15:09:19 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -12,19 +12,9 @@ public class Task1 {
         }
 
     public static int fibonacci(int n) {
-        int [] f =new int[n];
-        f [0] =0;
-        f [1] =1;
-        if(n==0||n==1){
-            return n;
-        }
-        else{
-            f[0]=1;
-            for(int i=2;i<n;i++){
-                f[i]=f[i-1]+f[i-2];
-            }
-        }
-        return f[n-1];
+       if(n==0||n==1)
+           return n;
+        else return fibonacci( n-1)+ fibonacci(n-2)
 
     }
     public static void main(String[] args){
