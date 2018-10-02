ca5e34ab3a0f163e48f95b6d34126d707b712a9c
MaiDat1 <43159622+MaiDat1@users.noreply.github.com>
2018-10-03 02:18:07 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -4,22 +4,46 @@ public class Week3 {
 
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        if (m >= n) return m;
+
+        else
+
+            return n;
     }
 
     public static int minOfArray(int[] array) {
-        // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        int temp;
+        for (int i = 0; i < array.length; i++)
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+        return array[0];
     }
 
     /**
      * Chương trình tính chỉ số BMI và in ra kết quả đánh giá
+     *
      * @param weight cân nặng
      * @param height chiều cao
      * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+
+        double bmi;
+        bmi = weight / (height * height);
+        if (bmi < 18.5)
+            return "Thieu can";
+        if (18.5 < bmi && bmi < 23)
+            return "Binh thuong";
+        else if (23 <= bmi && bmi < 24.99)
+            return "Thua can";
+        else
+            return "Beo phi";
     }
 }
