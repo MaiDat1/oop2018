0f9c66414c8ba20d8ff79429aed9e38837e2436b
MaiDat1 <43159622+MaiDat1@users.noreply.github.com>
2018-10-12 14:22:06 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -14,7 +14,7 @@ public class Task1 {
     public static int fibonacci(int n) {
        if(n==0||n==1)
            return n;
-        else return fibonacci( n-1)+ fibonacci(n-2)
+        else return fibonacci( n-1)+ fibonacci(n-2);
 
     }
     public static void main(String[] args){
